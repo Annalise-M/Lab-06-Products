2c3b8a98f2efa97ad81f5d99c55017cf131cd4d8
Annalise Murphy <nali247@gmail.com>
2020-04-06 16:05:17 -0700

    product data

---
+++ app.js
@@ -0,0 +1,10 @@
+import exoticfruits from './data.js';
+
+const container = document.getElementById('container');
+
+
+
+// // const noseRing = {
+//     id: 'noseRing'
+// }
+// button.value = 'noseRing'
---
+++ data.js
@@ -0,0 +1,76 @@
+
+
+const kumquat = {
+    id: 'item1',
+    name: 'kumquat',
+    image: 'https://cdn.shopify.com/s/files/1/2336/3219/products/Meiwa_Kumquat_x850.jpg?v=1525725643',
+    description: 'Nature\'s sour candy!',
+    category: 'fruit',
+    price: 11.99
+};
+
+const dragonfruit = {
+    id: 'item2',
+    name: 'dragonfruit',
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTPll4W-Yz9Tr5F9uROjOhmutV5ODLYQ6nY1CBdc2pfC_ETgebZ&usqp=CAU',
+    description: 'Also known as Pitaya!',
+    category: 'fruit',
+    price: 5.99
+}; 
+
+const mangosteen = {
+    id: 'item3',
+    name: 'mangosteen',
+    image: 'https://images-na.ssl-images-amazon.com/images/I/41nDqKsRGHL._SX425_.jpg',
+    description: 'Often called, the Queen of fruits!',
+    category: 'fruit',
+    price: 4.99
+};
+
+const durian = {
+    id: 'item4',
+    name: 'durian',
+    image: 'https://cdn1.i-scmp.com/sites/default/files/styles/768x768/public/images/methode/2018/04/29/4241cf12-4b60-11e8-85b3-af25d27017e0_1280x720_120045.JPG?itok=tM-o-d52',
+    description: 'Often called, the King of fruits!',
+    category: 'fruit',
+    price: 12.00
+};
+
+const passionfruit = {
+    id: 'item5',
+    name: 'passionfruit',
+    image: 'https://plantogram.com/wa-data/public/shop/products/00/04/400/images/1176/1176.750x0.jpg',
+    description: 'A vine species of passion flower!',
+    category: 'fruit',
+    price: 8.99
+};
+
+const starfruit = {
+    id: 'item6',
+    name: 'starfruit',
+    image: 'https://miro.medium.com/max/400/0*nnPSLKRiE89H4bkn.jpg',
+    description: 'Also known as the Carambola',
+    category: 'fruit',
+    price: 10.99
+};
+
+const monkfruit = {
+    id: 'item7',
+    name: 'monkfruit',
+    image: 'https://specialtyproduce.com/sppics/8824.png',
+    description: 'It tastes better than it looks!',
+    category: 'fruit',
+    price: 18.99
+};
+
+const exoticFruits = [
+    kumquat,
+    dragonfruit,
+    mangosteen,
+    durian,
+    passionfruit,
+    starfruit,
+    monkfruit
+];
+
+export default exoticFruits;
